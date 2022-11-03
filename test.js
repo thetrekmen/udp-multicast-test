2c444e4714d1c6760b1b065590dedef033018fce
thetrekmen <jonespm1.dev@gmail.com>
2022-11-03 13:36:20 -0400

    changed a variable to be more descriptive.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import dgram from 'node:dgram';
 
 const client = dgram.createSocket('udp4');
-const IP_ADDRESS = '10.3.1.24'
+const LOCAL_IP_ADDRESS = '10.3.1.24'
 
 client.on('error', (err) => {
     console.log(`client error:\n${err.stack}`);
@@ -17,8 +17,8 @@ client.on('listening', () => {
     console.log(`client listening ${address.address}:${address.port}`);
 });
 
-client.bind(9722, IP_ADDRESS, function () {
-    client.addMembership('239.26.21.23', IP_ADDRESS);
+client.bind(9722, LOCAL_IP_ADDRESS, function () {
+    client.addMembership('239.26.21.23', LOCAL_IP_ADDRESS);
 });
 
 
