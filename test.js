579a96650151e305b9770501d018ef1b7530435d
thetrekmen <jonespm1.dev@gmail.com>
2022-12-30 08:51:10 -0500

    added a function that writes data to a file if multimedia

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-import dgram from 'node:dgram';
+const dgram = require('dgram');
+var fs = require('fs');
 
 
-const LOCAL_IP_ADDRESS = '172.26.44.44'
-// const MULTICAST_ADDRESS = '239.26.21.41'
-// const PORT = 9722
-const MULTICAST_ADDRESS = '239.192.21.12'
-const PORT = 60012
+const LOCAL_IP_ADDRESS = '10.3.1.44'
+const MULTICAST_ADDRESS = '239.26.21.51'
+const PORT = 9722
+// const MULTICAST_ADDRESS = '239.192.21.11'
+// const PORT = 60111
 
 const client = dgram.createSocket({ type: 'udp4', reuseAddr: true });
 
@@ -14,16 +15,26 @@ client.on('error', (err) => {
     client.close();
 });
 
+//writing to file
 client.on('message', (msg, rinfo) => {
-  console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log(msg);
+    var stream = fs.createWriteStream("udp-stream.flac", { 'flags': 'a' });
+    stream.once('open', function (fd) {
+        stream.write(msg);
+    });
 });
+
+//printing to console
+// client.on('message', (msg, rinfo) => {
+//     console.log(msg);
+// });
 
 client.on('listening', () => {
     const address = client.address();
     console.log(`client listening ${address.address}:${address.port}`);
 });
 
-client.bind(PORT, LOCAL_IP_ADDRESS, function () {
+client.bind(PORT, function () {
     client.addMembership(MULTICAST_ADDRESS, LOCAL_IP_ADDRESS);
 });
 
